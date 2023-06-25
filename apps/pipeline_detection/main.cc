51a873ff1d4074d057e272e9ba22b4a0f31c1045
having11 <gatoninja236@gmail.com>
2023-06-24 20:48:06 -0500

    initial commit

---
+++ apps/pipeline_detection/main.cc
@@ -0,0 +1,96 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdio>
+#include <vector>
+
+#include "libs/base/http_server.h"
+#include "libs/base/led.h"
+#include "libs/base/strings.h"
+#include "libs/base/utils.h"
+#include "libs/camera/camera.h"
+#include "libs/libjpeg/jpeg.h"
+#include "third_party/freertos_kernel/include/FreeRTOS.h"
+#include "third_party/freertos_kernel/include/task.h"
+#include "libs/base/wifi.h"
+
+namespace coralmicro {
+namespace {
+
+constexpr char kIndexFileName[] = "/coral_micro_camera.html";
+constexpr char kCameraStreamUrlPrefix[] = "/camera_stream";
+
+HttpServer::Content UriHandler(const char* uri) {
+  if (StrEndsWith(uri, "index.shtml") ||
+      StrEndsWith(uri, "coral_micro_camera.html")) {
+    return std::string(kIndexFileName);
+  } else if (StrEndsWith(uri, kCameraStreamUrlPrefix)) {
+    // [start-snippet:jpeg]
+    std::vector<uint8_t> buf(CameraTask::kWidth * CameraTask::kHeight *
+                             CameraFormatBpp(CameraFormat::kRgb));
+    auto fmt = CameraFrameFormat{
+        CameraFormat::kRgb,       CameraFilterMethod::kBilinear,
+        CameraRotation::k0,       CameraTask::kWidth,
+        CameraTask::kHeight,
+        /*preserve_ratio=*/false, buf.data(),
+        /*while_balance=*/true};
+    if (!CameraTask::GetSingleton()->GetFrame({fmt})) {
+      printf("Unable to get frame from camera\r\n");
+      return {};
+    }
+
+    std::vector<uint8_t> jpeg;
+    JpegCompressRgb(buf.data(), fmt.width, fmt.height, /*quality=*/75, &jpeg);
+    // [end-snippet:jpeg]
+    return jpeg;
+  }
+  return {};
+}
+
+void Main() {
+  printf("Camera HTTP Example!\r\n");
+  // Turn on Status LED to show the board is on.
+  LedSet(Led::kStatus, true);
+
+  CameraTask::GetSingleton()->SetPower(true);
+  CameraTask::GetSingleton()->Enable(CameraMode::kStreaming);
+
+  if (!WiFiTurnOn(/*default_iface=*/false)) {
+    printf("Unable to bring up WiFi...\r\n");
+    vTaskSuspend(nullptr);
+  }
+  if (!WiFiConnect(10)) {
+    printf("Unable to connect to WiFi...\r\n");
+    vTaskSuspend(nullptr);
+  }
+  if (auto wifi_ip = WiFiGetIp()) {
+    printf("Serving on: %s\r\n", wifi_ip->c_str());
+  } else {
+    printf("Failed to get Wifi Ip\r\n");
+    vTaskSuspend(nullptr);
+  }
+
+  HttpServer http_server;
+  http_server.AddUriHandler(UriHandler);
+  UseHttpServer(&http_server);
+
+  vTaskSuspend(nullptr);
+}
+}  // namespace
+}  // namespace coralmicro
+
+extern "C" void app_main(void* param) {
+  (void)param;
+  coralmicro::Main();
+}
