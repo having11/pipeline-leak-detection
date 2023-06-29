fff0015a6408c7c066ce2201ce1273bda77bfb3d
having11 <gatoninja236@gmail.com>
2023-06-28 20:39:03 -0500

    rotate and resize

--- apps/pipeline_detection/web/grab_frames.py
+++ apps/pipeline_detection/web/grab_frames.py
@@ -14,12 +14,15 @@ parser.add_argument('--prefix', help='Prefix the image with this string; helpful
 for categorizing image labels for ML', default='')
 parser.add_argument('--timeout', help='Timeout after waiting this many seconds for an image',
                     type=int, default=3)
+parser.add_argument('--rotate', help='Rotate captures by this many degrees', type=int,
+                    default=0)
+parser.add_argument('--resize', help='Resize to given size', metavar='WIDTH HEIGHT',
+                    type=int, nargs=2)
 args = parser.parse_args()
 
 URL = f'http://{args.ip_address}/camera_stream'
 DEST_FOLDER = f'captures'
 DELAY_SECONDS = 1 / args.fps
-
 if args.prefix != '':
     args.prefix += '_'
 
@@ -33,6 +36,9 @@ while True:
     try:
         print('Attempting to grab image...')
         im = Image.open(BytesIO(requests.get(URL, stream=True, timeout=args.timeout).content))
+        im = im.rotate(args.rotate)
+        if args.resize:
+            im = im.resize(args.resize)
         filename = f'{args.prefix}capture{time.time():.2f}.jpg'
         print(f'Saving image={filename}')
         im.save(path.join(full_path, filename))
