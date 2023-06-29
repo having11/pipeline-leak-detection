9c931983e069ecbf527b0871fad66dba63149f90
having11 <gatoninja236@gmail.com>
2023-06-28 20:12:16 -0500

    properly retrieve image

--- apps/pipeline_detection/web/grab_frames.py
+++ apps/pipeline_detection/web/grab_frames.py
@@ -1,12 +1,14 @@
 from PIL import Image
 import requests
 import sys
+from io import BytesIO
 import os, os.path as path
 import time
 
-URL = f'http://192.168.0.17/camera_stream'
+IP_ADDR = sys.argv[1]
+URL = f'http://{IP_ADDR}/camera_stream'
 DEST_FOLDER = f'captures'
-FPS = 10
+FPS = 5
 DELAY_SECONDS = 1 / FPS
 TIMEOUT_SECONDS = 3
 
@@ -19,8 +21,8 @@ if not path.exists(full_path):
 while True:
     try:
         print('Attempting to grab image...')
-        im = Image.open(requests.get(URL, stream=True, timeout=TIMEOUT_SECONDS).raw)
-        filename = f'capture{int(time.time)}.jpg'
+        im = Image.open(BytesIO(requests.get(URL, stream=True, timeout=TIMEOUT_SECONDS).content))
+        filename = f'capture{time.time():.2f}.jpg'
         print(f'Saving image={filename}')
         im.save(path.join(full_path, filename))
         time.sleep(DELAY_SECONDS)
