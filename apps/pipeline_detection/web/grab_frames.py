80452dbbc71703523cab35b34b97f38840c2721f
having11 <gatoninja236@gmail.com>
2023-06-28 20:27:37 -0500

    add cli

--- apps/pipeline_detection/web/grab_frames.py
+++ apps/pipeline_detection/web/grab_frames.py
@@ -4,16 +4,24 @@ import sys
 from io import BytesIO
 import os, os.path as path
 import time
+import argparse
 
-IP_ADDR = sys.argv[1]
-URL = f'http://{IP_ADDR}/camera_stream'
+parser = argparse.ArgumentParser()
+parser.add_argument('ip_address', help='Coral micro\'s IP address', metavar='IP_ADDRESS')
+parser.add_argument('-o', '--output', help='Folder to store captured images')
+parser.add_argument('--fps', help='Capture images at this rate per second', type=int, default=5)
+parser.add_argument('--prefix', help='Prefix the image with this string; helpful\
+for categorizing image labels for ML', default='')
+parser.add_argument('--timeout', help='Timeout after waiting this many seconds for an image',
+                    type=int, default=3)
+args = parser.parse_args()
+
+URL = f'http://{args.ip_address}/camera_stream'
 DEST_FOLDER = f'captures'
-FPS = 5
-DELAY_SECONDS = 1 / FPS
-TIMEOUT_SECONDS = 3
+DELAY_SECONDS = 1 / args.fps
 
 homepath = path.expanduser('~')
-full_path = path.join(homepath, DEST_FOLDER)
+full_path = args.output if args.output is not None else path.join(homepath, DEST_FOLDER)
 print(f'Using path={full_path} to store captures')
 if not path.exists(full_path):
     os.mkdir(full_path)
@@ -21,8 +29,8 @@ if not path.exists(full_path):
 while True:
     try:
         print('Attempting to grab image...')
-        im = Image.open(BytesIO(requests.get(URL, stream=True, timeout=TIMEOUT_SECONDS).content))
-        filename = f'capture{time.time():.2f}.jpg'
+        im = Image.open(BytesIO(requests.get(URL, stream=True, timeout=args.timeout).content))
+        filename = f'{args.prefix}capture{time.time():.2f}.jpg'
         print(f'Saving image={filename}')
         im.save(path.join(full_path, filename))
         time.sleep(DELAY_SECONDS)
